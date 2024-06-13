f3dae15c1a9a2f41fbda0acdf671a7da892fbd04
Renato Gutiérrez <gutierrez.renato2020@gmail.com>
2024-06-13 10:29:04 -0500

    Carga Inicial

---
+++ ConsolaDePrueba/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsolaDePrueba.Clases;
+using System;
+
+namespace ConsolaDePrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente { Id = 1, IBS = 2080, Cedula = "04171522117", Nombre = "Juan Aragán" };
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine(cliente.Nombre);
+            Console.WriteLine("Pulse una tecla para continuar...");
+            Console.Read();
+        }
+    }
+}
